7dfbd62d083093d702c5b69f42a3fb2a7ba0055b
boucara <traorebelco@gmail.com>
2017-09-14 12:13:21 +0200

    les tests unitaires des classes

---
+++ src/test/groovy/plateforme/PoisSpec.groovy
@@ -0,0 +1,18 @@
+package plateforme
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class PoisSpec extends Specification implements DomainUnitTest<Pois> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
