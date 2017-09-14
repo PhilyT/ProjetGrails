5bdfaa0a3c372c3d5cbeaa07a8eb7aa77a62b8e1
Tom <tom.phily@live.fr>
2017-09-14 10:28:15 +0200

    #3 Installation de sprong security et creation des classe Utilisateur et Role

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import grails.mbds.projet.UtilisateurPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    utilisateurPasswordEncoderListener(UtilisateurPasswordEncoderListener, ref('hibernateDatastore'))
 }
